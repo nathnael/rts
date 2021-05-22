5c8d103091b76dacf6c58aacce090c25f2969b56
Nathnael Getahun <contact.nathnael@gmail.com>
2021-05-22 16:16:37 +0300

    First init commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users
+  resources :permissions
+  resources :invoices
+  resources :vendors
+  
+  # put '/invoices/assign_invoice/:id', to: 'invoices#assign_invoice'
+
+  put 'users/:id/updatePermissions', to: 'users#updatePermissions'
+  get 'users/:id/user_permissions', to: 'users#user_permissions'
+
+  root to: 'dashboard#index'
+end
