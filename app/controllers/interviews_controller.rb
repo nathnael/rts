d7463d41f73bbc5edc2b6921fbc6a41a01281904
Nathnael Getahun <contact.nathnael@gmail.com>
2021-05-31 12:45:16 +0300

    Completed Interview show page

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -12,6 +12,7 @@ class InterviewsController < ApplicationController
   def show
     @position = Position.find_by_id(@interview.position_id)
     @created_by = User.find_by_id(@interview.created_by)
+    @interview_criteria = InterviewCriterium.includes(:skill,:interview).where( interview_id: @interview.id)
   end
 
   # GET /interviews/new
@@ -28,7 +29,7 @@ class InterviewsController < ApplicationController
   # POST /interviews.json
   def create
     interview_criteria_hash = interview_params[:interview_criteria][0...-1]
-    interview_criteria = interview_criteria_hash.collect { |h| InterviewCriterium.new(h)}
+    interview_criteria = interview_criteria_hash.collect { |h| InterviewCriterium.new({ skill_id: h[:skill_id], interview_id: @interview&.id, minimum_score: h[:minimum_score], created_by: current_user.id})}
 
     interview_map = interview_params.except(:interview_criteria)
 
@@ -54,29 +55,28 @@ class InterviewsController < ApplicationController
     @interview.modified_by = current_user.id
 
     interview_criteria_ids = @interview.interview_criteria.collect { |ic| ic.id }
-
+    
     interview_criteria_hash = interview_params[:interview_criteria][0...-1]
     
     deleted_interview_criteria_ids = interview_criteria_ids -  interview_criteria_hash.collect { |r| r[:id].to_i }
-
+    
     InterviewCriterium.where( :id => deleted_interview_criteria_ids).destroy_all 
-
-    interview_criteria = interview_criteria_hash.collect do |h|
-                        if h[:skill_id] != nil && h[:skill_id] != ""
-                          if h[:id] != nil
-                            InterviewCriterium.find(h[:id])
-                          else             
-                            InterviewCriterium.new(h)
-                          end
+    
+    interview_criteria = interview_criteria_hash.collect do |h|   
+                            
+                        if h[:id] != nil
+                          InterviewCriterium.find(h[:id])
+                        else             
+                          InterviewCriterium.new({ skill_id: h[:skill_id], interview_id: @interview.id, minimum_score: h[:minimum_score], modified_by: current_user.id})
                         end
                       end
-
+                        
     interview_map = interview_params.except(:interview_criteria)
 
     interview_map[:interview_criteria] = interview_criteria
 
     respond_to do |format|
-      if @interview.update(interview_params)
+      if @interview.update(interview_map)
         format.html { redirect_to @interview, notice: 'Interview was successfully updated.' }
         format.json { render :show, status: :ok, location: @interview }
       else
