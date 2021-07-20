22d012f7c8448b4f2abbf44df3860b087daca02d
Nathnael Getahun <contact.nathnael@gmail.com>
2021-07-20 17:03:33 +0300

    Started working on dashboard widgets

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,36 @@
 class DashboardController < ApplicationController
     def index 
+        @no_of_projects = Project.all.count
+        @no_of_clients = Client.all.count
+        @unfulfilled_projects = get_unfulfilled_projects()
+        @active_projects = Project.where.not(status: Project.statuses[:active]).count
+        @no_of_excellers = Exceller.all.count
+        already_assigned = ProjectRequirementExceller.where(current: true).pluck(:exceller_id)
+        @no_of_unassigned_excellers = Exceller.where(status_id: Exceller.status_ids[:active]).where.not(id: already_assigned).count
+    end
+
+    def get_unfulfilled_projects
+        projects = Project.joins(:client).all
+        open_project_progress = []
+        for project in projects do
+            project_requirements = ProjectRequirement.where(project_id: project.id)
+            no_required_excellers = 0
+            no_assigned_excellers = 0
+            for project_requirement in project_requirements do 
+                final_state = ProjectRequirementState.where(project_requirement_id: project_requirement.id).order(order: :desc).first
+                no_of_assigned_excellers = 0
+                if final_state.present?
+                    no_of_assigned_excellers = ProjectRequirementExceller.where(project_requirement_id: project_requirement.id, state_id: final_state.id).count
+                end                
+                no_required_excellers += project_requirement.amount.to_f
+                no_assigned_excellers += no_of_assigned_excellers.to_f
+            end
+            project_progress = (no_assigned_excellers.to_f / no_required_excellers.to_f).round(2) * 100
+            if project_progress < 100
+                open_project_progress << { id: project.id, project: project.name, client: project.client.name, start_date: project.start_date, end_date: project.end_date, required_excellers: no_required_excellers, assigned_excellers: no_assigned_excellers, progress: project_progress }
+            end
+        end
+        return open_project_progress
     end
 
     def load_data    
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
     has_many :project_requirements
+    belongs_to :client
     enum status: [:registered, :contract_signed, :resource_allocated, :initiated, :active, :suspended, :closed]
 end
